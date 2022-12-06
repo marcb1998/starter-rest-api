8a5aead1514900ec98f6e1ecc423726d139d9e30
marcb98 <marc.brouwer@student.hu.nl>
2022-12-06 11:41:46 +0100

    f

--- firebase.js
+++ firebase.js
@@ -1,21 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDQd2YzaAgIwPrCqDxFGCSKduVNMqlKEMI",
-  authDomain: "typ-game-9d06c.firebaseapp.com",
-  projectId: "typ-game-9d06c",
-  storageBucket: "typ-game-9d06c.appspot.com",
-  messagingSenderId: "1009486356994",
-  appId: "1:1009486356994:web:d5f15b454da3e84a55fd1b",
-  measurementId: "G-MJHTWDPBMQ"
-};
+const serviceAccount = require('./accountInformationKey.json')
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+export default function db();
--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
+
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const serviceAccount = require('./accountInformationKey.json')
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -9,6 +21,11 @@ app.get('/test', (req, res) => {
   console.log('/test')
   res.json({ msg: 'dit is een test'}).end()
 })
+
+app.get('/words', (req, async res => {
+  const snapshot = await db.collection('words').get();
+  res.json({ msg : 'all words'}).end()
+}))
 
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
